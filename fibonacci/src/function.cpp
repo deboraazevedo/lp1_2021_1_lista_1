6224831b8892f6b1b627e933cab2f67914292b32
Débora Azevedo <deboraazevedoo@gmail.com>
2021-06-23 23:23:41 -0300

    questao 1 100% testada

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -4,6 +4,26 @@ std::vector<unsigned int> fib_below_n( unsigned int n )
 {
     // TODO: adicione o seu código aqui.
 
-    // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+std::vector < unsigned int> fib_seq;
+if (n == 0) {
+    return fib_seq;
+}
+else if (n == 1)
+{
+    return fib_seq;
+}
+else if (n > 1) {
+    fib_seq.push_back(1);
+    fib_seq.push_back(1);
+    int i;
+    for (i = 2; i < n; i++) {
+        fib_seq.push_back(fib_seq[i-1] + fib_seq[i-2]);
+        if (fib_seq.back() >= n) {
+            fib_seq.pop_back();
+        }
+    }
+    
+    return fib_seq;
+
+}
 }
